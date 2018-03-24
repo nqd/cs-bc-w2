7e3d51ae2f1212ee8feff3acf1399f3e0c4fac30
nqd <dinh.nguyen@veriksystems.com>
2018-03-24 17:25:33 +0700

    toward store

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,18 @@
-var Web3 = require("../node_modules/web3/");
+var Web3 = require("../node_modules/web3/")
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-var Escrow = artifacts.require("Escrow");
-var EscrowFactory = artifacts.require("EscrowFactory");
+var Escrow = artifacts.require("Escrow")
+var EscrowFactory = artifacts.require("EscrowFactory")
+var Store = artifacts.require("Store")
 
 module.exports = function (deployer) {
-  // deployer.deploy(
-  //   Escrow,
-  //   web3.eth.accounts[5],
-  //   { from: web3.eth.accounts[6], value: web3.toWei(1) }
-  // )
-  //   .then(() => {
-      deployer.deploy(
-        EscrowFactory,
-        { from: web3.eth.accounts[4] }
-      )
-    // })
-};
+  deployer.deploy(
+    Escrow,
+    web3.eth.accounts[5],
+    { from: web3.eth.accounts[6], value: web3.toWei(1) }
+  )
+  deployer.deploy(
+    EscrowFactory,
+    { from: web3.eth.accounts[4] }
+  )
+}
