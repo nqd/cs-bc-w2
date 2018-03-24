cf133089950d39ec79bce9cf769d82735c043221
nqd <dinh.nguyen@veriksystems.com>
2018-03-24 13:58:47 +0700

    second test

--- test/escrow.js
+++ test/escrow.js
@@ -1,6 +1,9 @@
 var Escrow = artifacts.require("./Escrow.sol");
 
 contract('Escrow', function (accounts) {
+  const buyer = accounts[6]
+  const seller = accounts[5]
+
   it("should create new contract", function () {
     let inst
     return Escrow.deployed().then(function (instance) {
@@ -17,6 +20,24 @@ contract('Escrow', function (accounts) {
       return inst.buyerOk()
     }).then(buyerOk => {
       assert.equal(buyerOk, false)
+    })
+  })
+
+  it('should allow both user aggree on the contract', () => {
+    let currentSellerBalance = web3.eth.getBalance(seller).toNumber()
+    let inst
+    return Escrow.deployed().then((instance) => {
+      inst = instance
+      return inst
+    }).then(_instance => {
+      // seller accept
+      return inst.accept(true, { from: seller })
+    }).then(_instance => {
+      inst.accept(true, { from: buyer }).then(() => {
+        return inst
+      })
+    }).then(_instance => {
+      expect(web3.eth.getBalance(seller).toNumber(), currentSellerBalance + web3.toWei(1))
     })
   })
 })
