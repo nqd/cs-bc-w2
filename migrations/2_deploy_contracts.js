d8ca0a500f9b19e32e14fa70b6fb695a01e12810
nqd <dinh.nguyen@veriksystems.com>
2018-03-24 15:23:33 +0700

    migrate EscrowFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,18 @@ var Web3 = require("../node_modules/web3/");
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
 var Escrow = artifacts.require("Escrow");
+var EscrowFactory = artifacts.require("EscrowFactory");
 
 module.exports = function (deployer) {
   deployer.deploy(
     Escrow,
     web3.eth.accounts[5],
     { from: web3.eth.accounts[6], value: web3.toWei(1) }
-  );
+  )
+    .then(() => {
+      deployer.deploy(
+        EscrowFactory,
+        { from: web3.eth.accounts[4] }
+      )
+    })
 };
